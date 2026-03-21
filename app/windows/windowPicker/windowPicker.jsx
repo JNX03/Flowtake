6aff458f89e0cda14e7cd5b20be652723cdc3e0a
Jnx03 <jn03official@gmail.com>
2026-03-21 23:40:55 +0700

    refactor: move windowPicker window to app/windows

--- app/windows/windowPicker/windowPicker.jsx
+++ app/windows/windowPicker/windowPicker.jsx
@@ -1,10 +1,10 @@
-import { initTauriBridge } from '../../src/tauriBridge'
+import { initTauriBridge } from '@shared/tauriBridge'
 initTauriBridge()
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '../../src/assets/index.css'
+import '@shared/assets/index.css'
 import App from "./App"
 
 const queryClient = new QueryClient()
