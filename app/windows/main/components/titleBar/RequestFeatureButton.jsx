fbebe552fbf30ca5c5de45e0d867b21b17d8d6e8
Jnx03 <jn03official@gmail.com>
2026-03-23 16:39:27 +0700

    fix: update feature request URL to GitHub issues

--- app/windows/main/components/titleBar/RequestFeatureButton.jsx
+++ app/windows/main/components/titleBar/RequestFeatureButton.jsx
@@ -1,7 +1,7 @@
 import { CodeBracketSquareIcon } from "@heroicons/react/16/solid"
 
 export default function RequestFeatureButton() {
-    return (<a className="mt-1 btn btn-xs" href="https://insigh.to/b/flowtake" target="_blank" rel="noreferrer">
+    return (<a className="mt-1 btn btn-xs" href="https://github.com/JNX03/Flowtake/issues" target="_blank" rel="noreferrer">
         <CodeBracketSquareIcon className="size-4" />
         Request a feature
     </a>)
